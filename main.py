262c0500adbbc1a0b37d43f38f706d6b67d10273
N0n3 <sultanovmirodil@mail.ru>
2021-02-04 17:40:50 +0500

    Made message capacity larger

--- main.py
+++ main.py
@@ -4,14 +4,12 @@ from sys import argv
 
 def encode(image, message):
     message = ''.join([bin(x)[2:].zfill(8) for x in bytes(message, 'utf-8')])
-    pixels = image.load()
-    max_avbl_size = image.size[0] * image.size[1] * 3
+    max_avbl_size = image.size[0] * image.size[1] * 4
     if len(message) > max_avbl_size:
         raise(f"Sorry, but only {(max_avbl_size)//8} bytes can be written to this image!")
     pixels = [list(x) for x in image.getdata()]
-    channels_len = len(pixels[0])
     for bit_idx in range(len(message)):
-        idx_x, idx_y = bit_idx // channels_len, bit_idx % channels_len
+        idx_x, idx_y = bit_idx // 4, bit_idx % 4
         if message[bit_idx] == '0' and pixels[idx_x][idx_y] % 2:
             pixels[idx_x][idx_y] -= 1
         elif message[bit_idx] == '1' and pixels[idx_x][idx_y] % 2 == 0:
@@ -24,18 +22,11 @@ def decode(image, length):
     message = ''
     pixels = [x for x in image.getdata()]
     data = []
-    channels_len = len(pixels[0])
-    if channels_len == 3:
-        for r, g, b in pixels:
-            data.append(r)
-            data.append(g)
-            data.append(b)
-    elif channels_len == 4:
-        for r, g, b, a in pixels:
-            data.append(r)
-            data.append(g)
-            data.append(b)
-            data.append(a)
+    for r, g, b, a in pixels:
+        data.append(r)
+        data.append(g)
+        data.append(b)
+        data.append(a)
     for i in range(length):
        message += str(data[i] % 2)
     return int(message, 2).to_bytes((len(message) + 7) // 8, byteorder='big').decode('utf-8')
@@ -44,12 +35,10 @@ def decode(image, length):
 if len(argv) < 2:
     exit("Lack of arguments")
 fname = input("Input image path: ")
-image = Image.open(fname)
+image = Image.open(fname).convert('RGBA')
 if argv[1] in ('-e', '--encode'):
     message = input("Input secret message: ")
-    print([x for x in image.getdata()][:100])
     image.putdata(encode(image, message))
-    print([x for x in image.getdata()][:100])
     fname = input("Input output file name (without extension): ") + '.png'
     image.save(fname, format='PNG')
     with open(fname, 'ab') as f:
